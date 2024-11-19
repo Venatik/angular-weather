a5570a8ab42d2805eb5b1e17bb66e173e3bb6385
Venatik <stefan.trajkovski008@gmail.com>
2024-11-19 12:15:56 +0100

    fix

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { WeatherData } from "../types/weather.interface";
-import { environment } from "../../environment";
+import { environment } from "../../environments/environment";
 
 @Injectable({
   providedIn: "root",
